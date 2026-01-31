c45a8e4c59a968251edd958336205f221929f80b
cataliiin <cataliiin9@gmail.com>
2026-01-31 17:57:19 +0200

    chore(backend): add PROJECT_NAME, PROJECT_DESCRIPTION, DEBUG to config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,3 +9,6 @@ SECRET_KEY = os.getenv("SECRET_KEY", "snaddad1asvxdcasdas9uasdnu9asd")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
+PROJECT_NAME = os.getenv("PROJECT_NAME", "Drivium")
+PROJECT_DESCRIPTION = os.getenv("PROJECT_DESCRIPTION", "Google Drive clone")
+DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t")
