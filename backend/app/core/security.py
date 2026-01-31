f914185765a7f6983bb7eb61e6207bca98880744
cataliiin <cataliiin9@gmail.com>
2026-01-31 21:08:44 +0200

    feat(backend): added security, JWT and password hashing

---
+++ backend/app/core/security.py
@@ -0,0 +1,33 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from schemas.auth import TokenData
+from core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+# Password Hashing
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+# JWT Token 
+def create_access_token(token_data: TokenData, expires_delta: timedelta | None = None) -> str:
+    if expires_delta:
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode = token_data.model_dump()
+    to_encode["exp"] = expire
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_token(token: str) -> TokenData | None:
+    try:
+        payload_dict = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return TokenData(**payload_dict)
+    except JWTError:
+        return None
