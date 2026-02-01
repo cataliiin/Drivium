90c0f90c478a7d2c8deb1868b799c62b348c6d1d
cataliiin <cataliiin9@gmail.com>
2026-02-01 03:04:09 +0200

    refactor(backend): changed register and login route to use service dependencies instead of global instance

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,11 +1,11 @@
 from fastapi import Depends, routing
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user import user_service
-from app.database.database import get_db
+from app.services.user import UserService
+from app.dependencies import get_db, get_user_service
 from sqlalchemy.orm import Session
 
 router = routing.APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/register", response_model=UserResponse)
-async def register(user_data: UserCreate, db: Session = Depends(get_db)):
+async def register(user_data: UserCreate, db: Session = Depends(get_db), user_service: UserService = Depends(get_user_service)):
     return user_service.create_user(user_data, db)
