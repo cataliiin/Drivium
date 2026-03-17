969d2d206e909771cccb7f34349d90be67a1584c
cataliiin <cataliiin9@gmail.com>
2026-03-17 10:59:24 +0200

    feat(frontend): 404 error page design

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,1 +1,8 @@
-<h1>404 Page Not Found</h1>
+<script>
+    import './layout.css';
+</script>
+
+<div class="flex justify-center min-h-screen items-center">
+    <p class="text-4xl font-bold">404</p>
+    <p class="text-4xl">-page-not-found</p>
+</div>
