06adbe57c289cb4f5c9608c699d66a002f98b71a
cataliiin <cataliiin9@gmail.com>
2026-03-17 18:52:53 +0200

    feat(frontend): modified logic for logged in check for protected routes

--- frontend/src/routes/(app-protected)/+layout.server.ts
+++ frontend/src/routes/(app-protected)/+layout.server.ts
@@ -2,14 +2,10 @@ import { redirect } from '@sveltejs/kit';
 
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ parent }) => {
-  const parent_data = await parent() as App.UserData;
-
-  if (parent_data.user_data && !parent_data.user_data.is_logged_in) {
+export const load: LayoutServerLoad = async ({ locals }) => {
+  if (!locals.is_logged_in) {
     throw redirect(302, '/login');
   }
-
-  return {
-    ...parent_data
-  };
+  
+  return {};
 };
