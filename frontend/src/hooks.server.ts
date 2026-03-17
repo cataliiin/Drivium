8cb4ce6afdd5482a248231c9b3303ad842816473
cataliiin <cataliiin9@gmail.com>
2026-03-17 16:18:32 +0200

    fix(frontend): move hooks from routes to src

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,12 +3,12 @@ import { JWT_SECRET_KEY } from '$env/static/private';
 import jwt from 'jsonwebtoken';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.is_logged_in = !!event.cookies.get('jwt');
+	event.locals.is_logged_in = !!event.cookies.get('access_token');
 
 	if (event.locals.is_logged_in) {
 		try {
 			const payload = jwt.verify(
-				event.cookies.get('jwt')!, 
+				event.cookies.get('access_token')!, 
 				JWT_SECRET_KEY!
 			) as any;
 			event.locals.user = {
@@ -16,7 +16,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 				username: payload.username
 			};
 		} catch {
-			event.cookies.delete('jwt', { path: '/' });
+			event.cookies.delete('access_token', { path: '/' });
 			event.locals.is_logged_in = false;
 		}
 	}
